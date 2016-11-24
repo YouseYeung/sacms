9f35190ca3df183f6d7c55a1b4179e02a4663e6a
William Ho <ho@WMA.lan>
2016-11-24 11:00:08 +0800

    modified admin.py to only allow leader to CRUD her own ResearchGroup

--- sacms/admin.py
+++ sacms/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
 from .models import Article, ResearchGroup
 
+# Admin class for Article
+class ArticleAdmin(admin.ModelAdmin):
+    pass
+
+# Admin class for ResearchGroup
+class ResearchGroupAdmin(admin.ModelAdmin):
+    def get_queryset(self, request):
+        qs = super(ResearchGroupAdmin, self).get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        # only list those groups whose leader is the current user
+        return qs.filter(leader=request.user)
+
 # Register Article class in admin site
-admin.site.register(Article)
+admin.site.register(Article, ArticleAdmin)
 # Register ResearchGroup class in admin site
-admin.site.register(ResearchGroup)
+admin.site.register(ResearchGroup, ResearchGroupAdmin)
